888b08397b534df2cd999964e7c808e136edd9ee
Trum-ok <artamarkan@gmail.com>
2024-12-01 03:13:46 +0300

    тексты вынесены в отдельный файл

---
+++ dev/text.py
@@ -0,0 +1,17 @@
+CANT = \
+"К сожалению я не умею поддерживать беседу, НО я умею генерировать **отличный код-ревью** Ваших проектов. Просто пришлите мне файл/архив проекта!"
+
+START = \
+"""
+Привет! Я - инструмент для проведения **код-ревью** проектов
+Для генерации .md файла просто пришлите мне файл/архив с Вашим проектом.
+На данный момент поддерживаются `.py`, `.js`, `.cs` файлы
+"""
+
+BLESS = \
+[
+    "CAACAgIAAxkBAAENP7RnSztPpbPClBhx7cjFsJ6eYuS4XgACsEsAAjDv2EqXgVR_6BNX8TYE",
+    "CAACAgIAAxkBAAENP7VnSztPfeJVRJPQhhkSEwABDTaDq04AAgsbAAITTclLKxSabHnDqPM2BA",
+    "CAACAgIAAxkBAAENP7ZnSztPp66HlbLKVgqaQ7ymuNefcwACVBQAAj7dWUgW8NCGj4ZkcDYE",
+    "CAACAgIAAxkBAAENP7pnSzt300ARHfklMlDLyzqkpeJydQACcCEAAoML8Et1u0psXtFPjzYE"
+]
